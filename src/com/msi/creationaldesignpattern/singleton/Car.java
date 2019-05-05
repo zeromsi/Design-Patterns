c1c86978bdfb3d4a1406d40c8956befba82b36f5
Syed Mainul Hasan <mainuls18@gmail.com>
2019-05-05 23:54:20 +0600

    Breach of Singleton pattern by instantiating with reflection API

--- src/com/msi/creationaldesignpattern/singleton/Car.java
+++ src/com/msi/creationaldesignpattern/singleton/Car.java
@@ -4,7 +4,12 @@ public class Car {
 	
 	private static volatile Car instance=null;
 	
-	private Car() {}
+	
+	private Car() {
+		if (instance != null) {
+			throw new RuntimeException("Instance already initialized. Please call getInstance()");
+		}
+	}
 	
 	public static Car getInstance() {
 		if(instance==null) {
--- src/com/msi/creationaldesignpattern/singleton/Client.java
+++ src/com/msi/creationaldesignpattern/singleton/Client.java
@@ -1,13 +1,23 @@
 package com.msi.creationaldesignpattern.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.msi.creationaldesignpattern.singleton.Car;
+
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Car objectOne = Car.getInstance();
 		Car objectTwo = Car.getInstance();
-		Car objectThree = Car.getInstance();
 		System.out.println(objectOne);
 		System.out.println(objectTwo);
+		
+		// Breaching with reflection API
+		Class clazz  = Class.forName("com.msi.creationaldesignpattern.singleton.Car");
+		Constructor<Car> ctor = clazz.getDeclaredConstructor();
+		ctor.setAccessible(true);
+		Car objectThree = ctor.newInstance();
 		System.out.println(objectThree);
 	}
 	
